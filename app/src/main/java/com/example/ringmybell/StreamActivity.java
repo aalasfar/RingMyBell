e1b13cce02d321ad86a0fbd15c71df5292d34389
Abdullah Alasfar <aalasfar@uci.edu>
2020-02-19 01:32:26 -0800

    Abdul
    Open Google

--- app/src/main/java/com/example/ringmybell/StreamActivity.java
+++ app/src/main/java/com/example/ringmybell/StreamActivity.java
@@ -22,11 +22,17 @@ public class StreamActivity extends AppCompatActivity {
         //getSupportActionBar().setTitle("Live Streaming");
 
         browser=(WebView) findViewById(R.id.webview);
-        link = (EditText) findViewById(R.id.editTextLink);
-        btn_visit=(Button) findViewById(R.id.btn_view);
+        //link = (EditText) findViewById(R.id.editTextLink);
+        //btn_visit=(Button) findViewById(R.id.btn_view);
 
         browser.setWebViewClient(new myWebViewClient());
 
+        String url = "https:www.google.com";
+        browser.getSettings().setLoadsImagesAutomatically(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+
+        browser.loadUrl(url);
+        /*
         btn_visit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +43,7 @@ public class StreamActivity extends AppCompatActivity {
 
                 browser.loadUrl(url);
             }
-        });
+        });*/
     }
 
     private class myWebViewClient extends WebViewClient {
@@ -49,3 +55,34 @@ public class StreamActivity extends AppCompatActivity {
         }
     }
 }
+
+
+/*
+<LinearLayout
+        android:layout_width="445dp"
+        android:layout_height="36dp"
+        android:layout_weight="1"
+
+        tools:ignore="MissingConstraints">
+
+
+    </LinearLayout>
+
+ <EditText
+            android:id="@+id/editTextLink"
+            android:layout_width="296dp"
+            android:layout_height="61dp"
+            android:layout_weight="9"
+            android:hint="Enter Website" />
+<Button
+            android:id="@+id/btn_view"
+            android:layout_width="121dp"
+            android:layout_height="67dp"
+            android:layout_weight="1"
+            android:backgroundTint="@color/colorPrimary"
+            android:text="Click"
+            android:textColor="#ffffff"
+            android:textSize="18sp" />
+
+
+* */
